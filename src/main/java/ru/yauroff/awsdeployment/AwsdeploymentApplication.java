2feac8bd2769fb1df68f4d6569165b632d80ee0e
lasonoff <lason-vrn@yandex.ru>
2021-08-30 22:29:35 +0300

    first commit

---
+++ src/main/java/ru/yauroff/awsdeployment/AwsdeploymentApplication.java
@@ -0,0 +1,13 @@
+package ru.yauroff.awsdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsdeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsdeploymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/yauroff/awsdeployment/AwsdeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yauroff.awsdeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsdeploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
