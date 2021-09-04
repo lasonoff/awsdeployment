637003c851304b012032039e41b90a614862acbe
lasonoff <lason-vrn@yandex.ru>
2021-09-04 21:51:19 +0300

    feat: add users rest api

--- src/test/java/ru/yauroff/awsdeployment/AwsdeploymentApplicationTests.java
+++ src/test/java/ru/yauroff/awsdeployment/AwsdeploymentApplicationTests.java
@@ -1,13 +1,7 @@
 package ru.yauroff.awsdeployment;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AwsdeploymentApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
