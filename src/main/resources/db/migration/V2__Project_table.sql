81434222faa0b6c9161229c2f915b8bc7e194f42
lasonoff <lason-vrn@yandex.ru>
2021-09-23 08:16:47 +0300

    fix(sql): fix column name path

--- src/main/resources/db/migration/V2__Project_table.sql
+++ src/main/resources/db/migration/V2__Project_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE `project`
     `name`        varchar(200) DEFAULT NULL,
     `description` varchar(200) DEFAULT NULL,
     `status`      varchar(60) NOT NULL,
-    'path'        varchar(200) DEFAULT NULL,
+    `path`        varchar(200) DEFAULT NULL,
     `url`         varchar(200) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
